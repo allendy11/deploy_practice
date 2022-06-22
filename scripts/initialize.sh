6a658da64e1c084edd44edbf4d78875e7b3ba183
Daehee Yoon <allendy11@gmail.com>
2022-06-22 11:20:49 +0900

    [server] fix scripts for test 5

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -4,11 +4,11 @@ cd /home/ubuntu/deploy_practice/server
 sudo apt-get update
 sudo apt-get install npm -y
 sudo npm install
-# sudo npm install pm2@latest -g
-# sudo apt-get install authbind
-# sudo touch /etc/authbind/byport/80
-# sudo chown ubuntu /etc/authbind/byport/80
-# sudo chmod 755 /etc/authbind/byport/80
+sudo npm install pm2@latest -g
+sudo apt-get install authbind
+sudo touch /etc/authbind/byport/80
+sudo chown ubuntu /etc/authbind/byport/80
+sudo chmod 755 /etc/authbind/byport/80
 
 
 
